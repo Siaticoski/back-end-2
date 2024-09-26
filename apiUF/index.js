8456dc32c0a39db0e46557d500b6b1b668f1cd44
Siaticoski <caiosiaticoski1@gmail.com>
2024-09-26 16:53:51 -0400

    api 3ºbimestre

--- apiUF/index.js
+++ apiUF/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const colecaoUF = require('./dados/dados.js');
+const colecaoUf = require('./dados/dados.js');
 
 const app = express();
 
-app.get('/ufs', (req, res) =>{
-    res.json(colecaoUF.colecaoUf)
+app.get('/ufs', (req, res) => {
+    res.json(colecaoUf)
+});
+
+app.get('/ufs/:iduf', (req, res) =>{
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.colecaoUf.find(u => u.id === idUF);
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+    }   else {
+        mensagemErro = 'Requisição inválida';
+    }
+
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).json({ "erro": mensagemErro})
+    }
 });
 
 app.listen(8080, () => {
